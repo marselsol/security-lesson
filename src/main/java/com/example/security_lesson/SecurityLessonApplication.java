5326bb8c793684136b7f280c8b110da4ad4c9b99
Marsel Gaisin <marselgaisin@gmail.com>
2025-09-07 14:47:19 +0300

    init

---
+++ src/main/java/com/example/security_lesson/SecurityLessonApplication.java
@@ -0,0 +1,13 @@
+package com.example.security_lesson;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecurityLessonApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SecurityLessonApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/security_lesson/SecurityLessonApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.security_lesson;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecurityLessonApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
